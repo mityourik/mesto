bc7606330ba957e37bfb8c620281e5e94a3eaf3d
Dmitry <mityourik@gmail.com>
2023-05-25 21:15:34 -0300

    additional fix JS review_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,119 @@
-// Добавление заданных карточек на страницу
-const templateProfile = document.querySelector('.template-cell');
-const elementsContainer = document.querySelector('.elements__cards');
+const templateItem = document.querySelector('.template-cell');
+const elementsItem = document.querySelector('.elements__cards');
 
-function createCard(name, link) {
-  const cardElement = templateProfile.content.cloneNode(true);
-  const cardImage = cardElement.querySelector('.elements__photo');
-  const cardTitle = cardElement.querySelector('.elements__title');
-  const likeButton = cardElement.querySelector('.elements__like-button');
+function renderNewCard(name, link) {
+  const cloneDefaultCard = templateItem.content.cloneNode(true);
+  const defaultCardImage = cloneDefaultCard.querySelector('.elements__photo');
+  const defaultCardName = cloneDefaultCard.querySelector('.elements__title');
+  const defaultCardLikeButton = cloneDefaultCard.querySelector('.elements__like-button');
+  const defaultCardTrashButton = cloneDefaultCard.querySelector('.elements__trash-button');
 
-  cardImage.src = link;
-  cardImage.alt = name;
-  cardTitle.textContent = name;
+  defaultCardImage.src = link;
+  defaultCardImage.alt = 'Изображение ' + name;
+  defaultCardName.textContent = name;
 
-  likeButton.addEventListener('click', () => {
-    likeButton.classList.add('elements__like-image_enabled');
+  defaultCardLikeButton.addEventListener('click', () => {
+    defaultCardLikeButton.classList.toggle('elements__like-image_enabled');
   });
 
-  return cardElement;
-}
-
-function renderCard(cardElement) {
-  elementsContainer.prepend(cardElement);
-}
-
-function addCardsToPage(cards) {
-  cards.forEach((card) => {
-    const cardElement = createCard(card.name, card.link);
-    renderCard(cardElement);
+  defaultCardTrashButton.addEventListener('click', () => {
+    const parentCard = defaultCardTrashButton.closest('.elements__cell');
+    parentCard.remove();
   });
+
+  defaultCardImage.addEventListener('click', () => {
+    openPopupContentPreview();
+    previewImage.src = link;
+    previewImage.alt = 'Изображение ' + name;
+    previewName.textContent = name;
+  });
+
+  return cloneDefaultCard;
 }
 
-addCardsToPage(initialCards);
+// Проходимся по массиву карточек
+initialCards.forEach(function(card) {
+  const cardElement = renderNewCard(card.name, card.link);
+  elementsItem.appendChild(cardElement);
+});
 
-// Попап редактирования профиля
+// Пользовательские карточки через popup_content_cell
+const buttonAddNewCell = document.querySelector('.profile__add-icon');
+const popupContentCell = document.querySelector('.popup_content_cell');
+const buttonClosePopupCell = document.querySelector('.button_close_cell');
+
+// Функция для открытия попап-окна
+function openPopupContentCell() {
+  popupContentCell.classList.add('popup_opened');
+}
+
+// Функция для закрытия попап-окна
+function closePopupContentCell() {
+  popupContentCell.classList.remove('popup_opened');
+}
+
+// Обработчик события для открытия попапа
+buttonAddNewCell.addEventListener('click', openPopupContentCell);
+
+// Обработчик события для закрытия попапа
+buttonClosePopupCell.addEventListener('click', closePopupContentCell);
+
+const formCreateCell = document.querySelector('[name="elements-form"]');
+const inputNameCell = formCreateCell.querySelector('[name="elements_input_name"]');
+const inputLinkCell = formCreateCell.querySelector('[name="elements_input_link"]');
+
+formCreateCell.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const name = inputNameCell.value;
+  const link = inputLinkCell.value;
+  
+  // Создать новую карточку с использованием значений из полей формы
+  const newCard = renderNewCard(name, link);
+  
+  // Добавить новую карточку в начало списка
+  elementsItem.prepend(newCard);
+  
+  // Закрыть попап
+  closePopupContentCell();
+  
+  // Очистить значения полей формы
+  formCreateCell.reset();
+});
+
+// закрыть окно попапа при клике вне окна
+popupContentCell.addEventListener('click', (evt) => {
+  if (evt.target === evt.currentTarget) {
+    closePopupContentCell(popupContentCell);
+    formCreateCell.reset();
+  }
+});
+
+// Предпросмотр фото карточки во всплывающем окне
+const popupContentPreview = document.querySelector('.popup_content_preview');
+const previewImage = popupContentPreview.querySelector('.popup__image-preview');
+const previewName = popupContentPreview.querySelector('.popup__preview-name');
+const buttonClosePreview = document.querySelector('.button_close_preview');
+
+// Функция для открытия попап-окна превью
+function openPopupContentPreview() {
+  popupContentPreview.classList.add('popup_opened');
+}
+
+// Функция для закрытия попап-окна превью
+function closePopupContentPreview() {
+  popupContentPreview.classList.remove('popup_opened');
+}
+
+buttonClosePreview.addEventListener('click', closePopupContentPreview);
+
+// закрыть окно попапа при клике вне окна
+popupContentPreview.addEventListener('click', (evt) => {
+  if (evt.target === evt.currentTarget) {
+    closePopupContentPreview(popupContentCell);
+  }
+});
+
+// ----- Попап редактирования профиля ПР#4 ----- //
 const buttonPopupOpen = document.querySelector('.profile__popup-open');
 const popupProfile = document.querySelector('.popup_content_profile');
 const profileCloseButton = popupProfile.querySelector('.popup__close-button');
@@ -72,92 +153,5 @@ popupProfile.addEventListener('click', (evt) => {
   if (evt.target === evt.currentTarget) {
     closePopup(popupProfile);
     profileForm.reset();
-  }
-});
-
-// Добавление карточки "Место"
-const popupAddCell = document.querySelector('.profile__add-icon');
-const popupCreateCell = document.querySelector('.popup_content_cell');
-const popupCloseCell = document.querySelector('.button_close_cell');
-const formCreateCell = document.querySelector('[name="elements-form"]');
-const inputNameCell = formCreateCell.querySelector('[name="elements_input_name"]');
-const inputLinkCell = formCreateCell.querySelector('[name="elements_input_link"]');
-const buttonSaveCell = formCreateCell.querySelector('.popup__save-cell');
-
-popupAddCell.addEventListener('click', () => {
-  openPopup(popupCreateCell);
-});
-
-popupCloseCell.addEventListener('click', () => {
-  closePopup(popupCreateCell);
-  formCreateCell.reset();
-});
-
-popupCreateCell.addEventListener('click', (evt) => {
-  if (evt.target === evt.currentTarget) {
-    closePopup(popupCreateCell);
-    formCreateCell.reset();
-  }
-});
-
-formCreateCell.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const imageUrl = inputLinkCell.value;
-  const title = inputNameCell.value;
-
-  if (imageUrl && title) {
-    const cardElement = createCard(title, imageUrl);
-    renderCard(cardElement);
-    formCreateCell.reset();
-    closePopup(popupCreateCell);
-  }
-});
-
-// Удаление карточки
-elementsContainer.addEventListener('click', function(event) {
-  if (event.target.classList.contains('elements__trash-button')) {
-    const listItem = event.target.closest('.elements__cell');
-    listItem.remove();
-  }
-});
-
-// обработчик события для каждой кнопки "Нравится" после добавления пользовательских карточек
-const likeButtons = document.querySelectorAll('.elements__like-button');
-likeButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    button.classList.add('elements__like-image_enabled');
-  });
-});
-
-// Предпросмотр фото карточки во всплывающем окне
-const popupPreview = document.querySelector('.popup_content_preview');
-const previewImage = popupPreview.querySelector('.popup__image-preview');
-const previewName = popupPreview.querySelector('.popup__preview-name');
-const buttonClosePreview = document.querySelector('.button_close_preview');
-
-function togglePopupState(popup) {
-  popup.classList.toggle('popup_opened');
-}
-
-document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('elements__photo')) {
-    const clickedImageSrc = event.target.getAttribute('src');
-    const titleText = event.target.closest('.elements__cell').querySelector('.elements__title').textContent;
-
-    previewImage.setAttribute('src', clickedImageSrc);
-    previewName.textContent = titleText;
-
-    togglePopupState(popupPreview);
-  }
-});
-
-buttonClosePreview.addEventListener('click', () => {
-  togglePopupState(popupPreview);
-});
-
-popupPreview.addEventListener('click', (evt) => {
-  if (evt.target === evt.currentTarget) {
-    togglePopupState(popupPreview);
   }
 });

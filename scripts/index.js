a4d0dde87d7a4877858218f39fafdb0bf3fc6a36
Dmitry <mityourik@gmail.com>
2023-05-26 03:26:15 -0300

    fix JS double funcs review_2

--- scripts/index.js
+++ scripts/index.js
@@ -22,7 +22,7 @@ function renderNewCard(name, link) {
   });
 
   defaultCardImage.addEventListener('click', () => {
-    openPopupContentPreview();
+    openPopup(popupContentPreview);
     previewImage.src = link;
     previewImage.alt = 'Изображение ' + name;
     previewName.textContent = name;
@@ -43,20 +43,24 @@ const popupContentCell = document.querySelector('.popup_content_cell');
 const buttonClosePopupCell = document.querySelector('.button_close_cell');
 
 // Функция для открытия попап-окна
-function openPopupContentCell() {
-  popupContentCell.classList.add('popup_opened');
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
 // Функция для закрытия попап-окна
-function closePopupContentCell() {
-  popupContentCell.classList.remove('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 // Обработчик события для открытия попапа
-buttonAddNewCell.addEventListener('click', openPopupContentCell);
+buttonAddNewCell.addEventListener('click', () => {
+  openPopup(popupContentCell);
+});
 
 // Обработчик события для закрытия попапа
-buttonClosePopupCell.addEventListener('click', closePopupContentCell);
+buttonClosePopupCell.addEventListener('click', () => {
+  closePopup(popupContentCell);
+});
 
 const formCreateCell = document.querySelector('[name="elements-form"]');
 const inputNameCell = formCreateCell.querySelector('[name="elements_input_name"]');
@@ -66,16 +70,16 @@ formCreateCell.addEventListener('submit', (event) => {
   event.preventDefault();
   const name = inputNameCell.value;
   const link = inputLinkCell.value;
-  
+
   // Создать новую карточку с использованием значений из полей формы
   const newCard = renderNewCard(name, link);
-  
+
   // Добавить новую карточку в начало списка
   elementsItem.prepend(newCard);
-  
+
   // Закрыть попап
-  closePopupContentCell();
-  
+  closePopup(popupContentCell);
+
   // Очистить значения полей формы
   formCreateCell.reset();
 });
@@ -83,7 +87,7 @@ formCreateCell.addEventListener('submit', (event) => {
 // закрыть окно попапа при клике вне окна
 popupContentCell.addEventListener('click', (evt) => {
   if (evt.target === evt.currentTarget) {
-    closePopupContentCell(popupContentCell);
+    closePopup(popupContentCell);
     formCreateCell.reset();
   }
 });
@@ -94,22 +98,14 @@ const previewImage = popupContentPreview.querySelector('.popup__image-preview');
 const previewName = popupContentPreview.querySelector('.popup__preview-name');
 const buttonClosePreview = document.querySelector('.button_close_preview');
 
-// Функция для открытия попап-окна превью
-function openPopupContentPreview() {
-  popupContentPreview.classList.add('popup_opened');
-}
-
-// Функция для закрытия попап-окна превью
-function closePopupContentPreview() {
-  popupContentPreview.classList.remove('popup_opened');
-}
-
-buttonClosePreview.addEventListener('click', closePopupContentPreview);
+buttonClosePreview.addEventListener('click', () => {
+  closePopup(popupContentPreview);
+});
 
 // закрыть окно попапа при клике вне окна
 popupContentPreview.addEventListener('click', (evt) => {
   if (evt.target === evt.currentTarget) {
-    closePopupContentPreview(popupContentCell);
+    closePopup(popupContentPreview);
   }
 });
 
@@ -123,14 +119,6 @@ const profileNameField = profileForm.querySelector('[name="profile-input_name"]'
 const profileDescripField = profileForm.querySelector('[name="profile-input_description"]');
 const profileTitle = document.querySelector('.profile__title');
 const profileParagraph = document.querySelector('.profile__paragraph');
-
-const openPopup = (popupToOpen) => {
-  popupToOpen.classList.add('popup_opened');
-};
-
-const closePopup = (popupToClose) => {
-  popupToClose.classList.remove('popup_opened');
-};
 
 buttonPopupOpen.addEventListener('click', () => {
   openPopup(popupProfile);
@@ -152,6 +140,5 @@ profileForm.addEventListener('submit', (event) => {
 popupProfile.addEventListener('click', (evt) => {
   if (evt.target === evt.currentTarget) {
     closePopup(popupProfile);
-    profileForm.reset();
   }
 });
